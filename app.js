e424385c87e0aff9c888917db5bf43bec0798457
YumnaAsim <yumnaasim11@gmail.com>
2017-11-11 12:45:39 +0500

    node for beginners

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
 /*This file runs when we build our project*/
 console.log('Hello');
-console.log("yucca");
+
+var person = {
+    name:"yumna",
+    age:21
+}
+console.log(person);
+
+var myName = function () {
+    console.log("Yumna Asim");
+};
+myName();
+
+function myAge() {
+    console.log("Age is "+20);
+}
+myAge();
+
+function mySalary() {}
+console.log(mySalary());
+setTimeout(myAge,5000);
+
+function placeOrder(orderNumber) {
+    console.log("Customer order ",orderNumber);
+    cookAndDeliverFood(function () {
+       console.log("Delivered food number is "+orderNumber);
+    });
+}
+
+function cookAndDeliverFood(callback) {
+    setTimeout(callback,5000);
+}
+placeOrder(1);
+placeOrder(2);
+placeOrder(3);
+placeOrder(4);
+placeOrder(5);
+
+var Person={
+    favFood:"Biryani",
+    favDesert:"Custard"
+};
+console.log(Person);
+var Yumna = Person;
+Yumna.favFood = "Daleem";
+Yumna.favDesert = "Sweets";
+console.log(Yumna);
+console.log(Person);
+console.log(20=='21'); /*compares only values*/
+console.log('Yumna'==='Yumna'); /*compares values and data type*/
