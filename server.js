442a59bbcda8ace429a0a85f133563f410c37d3f
YumnaAsim <yumnaasim11@gmail.com>
2017-11-13 19:01:12 +0500

    created a basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+/*immporting http module*/
+var http = require('http');
+function onRequest(request,response) {
+    console.log("A User sends a request "+request.url);
+    response.writeHead(200,{"Context-Type" : "text/plain"});
+    response.write("Here is some data");
+    response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server is now running...");
