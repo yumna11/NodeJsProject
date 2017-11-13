2e33be3c595fc40df3ae1038e4605bdb8e4e9fc8
YumnaAsim <yumnaasim11@gmail.com>
2017-11-13 20:01:39 +0500

    created a web file server

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 /*immporting http module*/
 var http = require('http');
-function onRequest(request,response) {
-    console.log("A User sends a request "+request.url);
-    response.writeHead(200,{"Context-Type" : "text/plain"});
-    response.write("Here is some data");
+var fs = require('fs');
+//404 response
+function sendErrorResponse(response) {
+    response.writeHead(404,{"Context-Type":"text/plain"});
+    response.write("404, Page not found!");
     response.end();
+}
+//handle a user request
+function onRequest(request,response) {
+if (request.method == 'GET' && request.url == '/')
+{
+    response.writeHead(200,{"Context-Type":"text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+}
+else
+{
+    sendErrorResponse(response);
+}
 }
 http.createServer(onRequest).listen(8888);
 console.log("Server is now running...");
