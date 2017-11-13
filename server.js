c53080751b6085bbbd9053100ecbdc8b718c0658
YumnaAsim <yumnaasim11@gmail.com>
2017-11-13 22:32:24 +0500

    connect framework

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-/*immporting http module*/
+var connect = require('connect');
 var http = require('http');
-var fs = require('fs');
-//404 response
-function sendErrorResponse(response) {
-    response.writeHead(404,{"Context-Type":"text/plain"});
-    response.write("404, Page not found!");
-    response.end();
-}
-//handle a user request
-function onRequest(request,response) {
-if (request.method == 'GET' && request.url == '/')
-{
-    response.writeHead(200,{"Context-Type":"text/html"});
-    fs.createReadStream("./index.html").pipe(response);
-}
-else
-{
-    sendErrorResponse(response);
-}
-}
-http.createServer(onRequest).listen(8888);
-console.log("Server is now running...");
+
+var app = connect();
+
+http.createServer(app).listen(8888);
+console.log("Server is running ...");
